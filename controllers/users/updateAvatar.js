dff1c9ef0b2e9d14232bfe433567536987a49c16
SergiyenkoDY <85953081+lokomativniy@users.noreply.github.com>
2022-06-15 18:00:36 +0300

    hw5

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,16 +1,26 @@
 const { User } = require("../../models");
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
-const updateAvatar = async (req, res) => {
+const updateAvatar = async (req, res, next) => {
   const { path: tempUpload, originalname } = req.file;
   const { _id: id } = req.user;
   const imageName = `${id}_${originalname}`;
   try {
     const resultUpload = path.join(avatarsDir, imageName);
     await fs.rename(tempUpload, resultUpload);
+
+    Jimp.read(resultUpload)
+      .then((img) => {
+        return img.resize(250, 250).quality(60).write(resultUpload);
+      })
+      .catch((err) => {
+        next(err);
+      });
+
     const avatarURL = path.join("public", "avatars", imageName);
     await User.findByIdAndUpdate(req.user._id, { avatarURL });
     res.json({ avatarURL });
